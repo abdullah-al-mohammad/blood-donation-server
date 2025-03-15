7452c9f65037709b134976e7339db3b3ff643020
abdullah-al-mohammad <abdullahalmuhammad6@gmail.com>
2025-03-15 09:05:36 +0600

    add get data request

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ require('dotenv').config()
 const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // middleware
 app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3umb5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,6 +25,7 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    // collections
     const userCollection = client.db('bloodDonationDb').collection('users')
 
     app.post('/users', async(req,res) => {
